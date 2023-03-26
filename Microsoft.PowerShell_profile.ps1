a23528eaf22bdfe5cb1c9248283c6efeb984ff1a
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-26 22:34:56 +0530

    added figlet

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,8 @@
-<# 0.Microsoft.Profile_profile.ps1
+<# 
  ____                        _   _               
 / ___| _   _ _ __   ___ _ __| | | |___  ___ _ __ 
-\___ \| | | | '_ \ / _ \ '__| | | / __|/ _ \ '__|
- ___) | |_| | |_) |  __/ |  | |_| \__ \  __/ |   
+\___ \| | | | '_ \ / _ \ '__| | | / __|/ _ \ '__| 0.Microsoft.Profile_profile.ps1
+ ___) | |_| | |_) |  __/ |  | |_| \__ \  __/ |    v1.0 | 26th March 2023
 |____/ \__,_| .__/ \___|_|   \___/|___/\___|_|   
             |_|                                  
 ---------------------------------------------------------
@@ -23,7 +23,7 @@
     - My Github Repository                  (https://github.com/SanjeevStephan/superuser)
     - Microsoft Windows Terminal Repository (https://github.com/microsoft/terminal)
     - PowerShell v7+                        (https://github.com/PowerShell/PowerShell/releases/latest)
-    - PowerShell-7.3.3-win-x64.zip (https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/PowerShell-7.3.3-win-x64.zip)
+    - PowerShell-7.3.3-win-x64.zip          (https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/PowerShell-7.3.3-win-x64.zip)
 .NOTES
     .AUTHOR
         -> Sanjeev_Stephan_Murmu
@@ -31,10 +31,12 @@
     .FILES
         -> Microsoft.PowerShell_profile.ps1
         -> profile_config.ps1
-        -> profile_func.ps1
+        -> profile_exec_script.ps1
+        -> profile_function.ps1
+        -> profile_get_path.ps1
+        -> profile_get_script_path.ps1
         -> profile_include.ps1
-        -> profile_path.ps1
-        -> profile_path.ps1
+
 
     .CONTAINS
         -> configuration
@@ -43,25 +45,12 @@
         -> paths
         -> variables
         -> functions
-#-------------------- Include Below File --------------------------------------#>
-# Define the path to the configuration file that contains additional dependencies and settings
-$profile_config = "$Home\Documents\PowerShell\profile_config.ps1"
-$profile_path = "$Home\Documents\PowerShell\profile_get_path.ps1"
-$profile_include = "$Home\Documents\PowerShell\profile_include.ps1"
+#-------------------- Must Include Below File --------------------------------------#>
+. "$HOME\Documents\PowerShell\profile_include.ps1"
+#-------------------- Functions {Read-Only}-------------------------------------
 
-# Include the configuration file to load any additional dependencies
-#. $profile_config
-#. $profile_path 
-. $profile_include
-
-#-------------------- Functions {Read-Only}--------------
-#-------------------- Functions {Read-Only}--------------
-
-function initialize() {
-
-
-
-}
+function initialize() { }
 function debug($msg){ Write-Output "[DEBUG] $msg"}
+
 #-------------------- Script Execution Begins from here-----
 initialize  #calling the initialize() function 
