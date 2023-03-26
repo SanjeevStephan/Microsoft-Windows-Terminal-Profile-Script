01ec7d55ee8d4d8d1d709cee359b01ed2ac60184
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-26 22:36:11 +0530

    commented the function -> in order to disable the self-execution of the script

--- profile_get_script_path.ps1
+++ profile_get_script_path.ps1
@@ -7,33 +7,33 @@
                                       
                                                      
 .SYNOPSIS
-    Script to get_path other PowerShell scripts as dependencies
+    Script to get_script_path other PowerShell scripts as dependencies
 
 .DESCRIPTION
-    Execute the script_files that are 'get_pathd' in this 'profile_get_path.ps1' script
+    Execute the script_files that are 'get_script_pathd' in this 'profile_get_script_path.ps1' script
 
 .NOTES
     AUTHOR
         -Sanjeev Stephan Murmu
     SCRIPT
-        -profile_get_path.ps1
+        -profile_get_script_path.ps1
     VERSION
         -v1.0    
 
 
 
 
-#-------------------- get_path Below File --------------------------------------#>
+#-------------------- get_script_path Below File --------------------------------------#>
 # Define the path to the configuration file that contains additional dependencies
 $profile_config_path = "$Home\Documents\PowerShell\profile_config.ps1"
 
-# get_path the configuration file to load any additional dependencies
-#  . $profile_config_path  # <-------------- NOTE-1 : TO RUN THIS SCRIPT | UNCOMMENT 
+# get_script_path the configuration file to load any additional dependencies
+  . $profile_config_path  # <-------------- NOTE-1 : TO RUN THIS SCRIPT | UNCOMMENT 
 
- # Debug profile_get_path.ps1  
-if($DEBUG["debug_path"] -eq "enable"){ Write-Output "--------------------------{profile_get_path.ps1}-------------------------"} 
+ # Debug profile_get_script_path.ps1  
+if($DEBUG["debug_path"] -eq "enable"){ Write-Output "--------------------------{profile_get_script_path.ps1}-------------------------"} 
    
-# Define a JSON string that contains the names and paths of the dependencies to be get_pathd
+# Define a JSON string that contains the names and paths of the dependencies to be get_script_pathd
 # Read the JSON data from the file
 $profile_path_json_data = Get-Content -Path "$Home\Documents\PowerShell\scripts.json" -Raw
 
@@ -41,8 +41,8 @@ $profile_path_json_data = Get-Content -Path "$Home\Documents\PowerShell\scripts.
 $profile_path_array = ConvertFrom-Json -InputObject $profile_path_json_data 
 
 #-------------------- Functions {Read-Only | Don't Modify unless u know what u are doing}--------------
-# Function to get_path by specifying Script name
-function get_path($script_name)
+# Function to get_script_path by specifying Script name
+function get_script_path($script_name)
 {
     # Get the path to the Script based on its name
     $script_array_data = $profile_path_array | Where-Object { $_.Name -eq $script_name }
@@ -50,12 +50,12 @@ function get_path($script_name)
     # Write-Output "Script Name : $script_name | Path $script_path"
     # Check if the file exists before including it
     if (Test-Path $script_path){
-        # get_path the dependency file
+        # get_script_path the dependency file
         #. $script_path
 
             if(($DEBUG["debug_path"]) -eq "enable") 
             { Write-Output "Script Name : $script_name | Path $script_path" } 
-            else { <# Write-Output "[] 'Enable' the 'display_get_path_confirmation' in the $config_file" #> }
+            else { <# Write-Output "[] 'Enable' the 'display_get_script_path_confirmation' in the $config_file" #> }
     }
     else{
         Write-Error "Error: $script_name not found at path $script_path"
@@ -63,14 +63,14 @@ function get_path($script_name)
     return $script_path
 }
 
-# Function to get_path all dependencies specified in the JSON string
+# Function to get_script_path all dependencies specified in the JSON string
 function list_all_script(){
 
     if(($TABLE["show_all_scripts"]) -eq "enable") 
     {   #Write-Output "Script Name : $script_name | Path $script_path" 
         $profile_path_array | Sort-Object | Format-Table @{label="Serial"; expression={$profile_path_array.IndexOf($_) + 1}}, 
         @{label="Name"; expression={$_.Name}}, 
-        @{label="File"; expression={$_.File}},
+ #       @{label="File"; expression={$_.File}},
         @{label="Path"; expression={$_.Path}},
         @{label="Description"; expression={$_.Desc}} -AutoSize
     } 
@@ -80,6 +80,6 @@ function list_all_script(){
 
 }
 
-# get_path_Dependencies # get_path necessary dependencies for the Microsoft.PowerShell_Profile.ps1
- get_path("powershell") # <-------------- NOTE-2 : TO RUN THIS SCRIPT | UNCOMMENT 
- list_all_script          # <-------------- NOTE-3 : TO RUN THIS SCRIPT | UNCOMMENT 
+# get_script_path_Dependencies # get_script_path necessary dependencies for the Microsoft.PowerShell_Profile.ps1
+ # get_script_path("profile_exec_script") # <-------------- NOTE-2 : TO RUN THIS SCRIPT | UNCOMMENT 
+ # list_all_script          # <-------------- NOTE-3 : TO RUN THIS SCRIPT | UNCOMMENT 
