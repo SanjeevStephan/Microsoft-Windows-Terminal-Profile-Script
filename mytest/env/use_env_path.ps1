6ed5843bd3b185255c698faaaba9e5ab1ef03568
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-29 01:01:15 +0530

    removed

--- mytest/env/use_env_path.ps1
+++
@@ -1,8 +0,0 @@
-$path = "${env:USERPROFILE}\Documents\file.txt"
-$terminal = "${env:TERMINAL}\taskarray\"
-
-Write-Output "Path $path"
-
-dir $terminal
-cd ${env:TERMINAL}
-echo "Moved to the Terminal"
