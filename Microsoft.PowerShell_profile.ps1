c770f3d15b479bcdc8761d7f349473583dc38293
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-29 15:49:13 +0530

    fixed directory structure

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,13 +11,6 @@
 
 .DESCRIPTION
     This PowerShell Script comprises the index of all the functions and alias for the window terminal
-
-    .PREREQUISITES
-        -You must be running Windows 10 2004 (build >= 10.0.19041.0) or later to run Windows Terminal
-        -You must enable Developer Mode in the Windows Settings app to locally install and run Windows Terminal (https://docs.microsoft.com/en-us/windows/uwp/get-started/enable-your-device-for-development)
-        -You must have PowerShell 7 or later installed (https://github.com/PowerShell/PowerShell/releases/latest)
-        -You must have at least VS 2022 installed (https://visualstudio.microsoft.com/downloads/)
-        -You must install the .NET Framework Targeting Pack to build test projects (https://docs.microsoft.com/dotnet/framework/install/guide-for-developers#to-install-the-net-framework-developer-pack-or-targeting-pack)
         
 .LINK
     - My Github Repository                  (https://github.com/SanjeevStephan/superuser)
@@ -36,36 +29,10 @@
         -> include.ps1
         -> README.md
 
-    .SCRIPT
-        -> profile_config.ps1
-        -> profile_include.ps1
-        -> profile_exec_script.ps1
-        -> profile_function.ps1
-        -> profile_getDependencies.ps1
-        -> profile_getFunction.ps1
-        -> profile_getJson.ps1
-        -> profile_getPath.ps1
-        -> profile_getScript.ps1
-
-    .JSON
-        -> dependencies.json
-        -> directories.json
-        -> functions.json
-        -> include.json
-        -> jsons.json
-        -> scripts.json
-        
-     .DIRECTORY
-        -> myfunctions
-        -> myscripts
-        -> mytest       
-
     .CONTAINS
         -> configuration
         -> dictionary
         -> dependencies        
-        -> paths
-        -> variables
         -> functions
 
     .FILE_TYPE
@@ -73,6 +40,38 @@
         -> json_file.json
         -> markdown.md
 
+.DIRECTORY_STRUCTURE
+    $Home\Documents\PowerShell\
+    ├───lost
+    │   └───works-great-but-not-in-use
+    ├───myfunctions
+    │   └───parameterArgs
+    ├───myjson/
+        ├───.\dependencies.json
+        ├───.\directories.json
+        ├───.\myFunctions.json
+        ├───.\myIncludes.json
+        ├───.\myJsonList.json
+        ├───.\myPythonScript.json
+        ├───.\myScripts.json
+        └───.\profileFunctions.json
+    ├───mypython
+        └───.\figlet.py    
+    ├───myscripts
+        └──────setEnv
+               ├───.\argsAddVariableToEnv.ps1
+               └───.\passArgsViaHashTable.ps1               
+        ├───.\execScript.ps1
+        ├───.\getDependencies.ps1
+        ├───.\getFunction.ps1
+        ├───.\getJson.ps1
+        ├───.\getPath.ps1
+        ├───.\getPython.ps1
+        ├───.\getScript.ps1
+        ├───.\setEnv.ps1
+        └───.\setMyfunctions.ps1
+    └───mytest
+        └───env            
 
 #-------------------- Must Include Below File --------------------------------------#>
 . "$HOME\Documents\PowerShell\include_dependecies.ps1"
@@ -80,12 +79,14 @@
 
 function initialize() {
 
-    & python "D:\terminal\py\figlet.py" --message 'SuperUser'
+    Check-File all
+    
+    & python "$HOME\Documents\PowerShell\mypython\figlet.py" --message 'SuperUser'
     # initial_debug 
     if($DEBUG[1]["Status"] -eq "enable") 
     { 
         Show-Config
-        Check-File all # Test-Path Of all the dependencies
+         # Test-Path Of all the dependencies
     }
     Home # Change Directory 'cd' to $Home\Documents\PowerShell
 
