44bd2cb86a4fa92c44613535613e0f8c70b1d008
Sanjeev Stephan <samstephan164@gmail.com>
2023-03-30 06:26:20 +0530

    Added 'Goto-Directory()' function

--- myfunctions/goto.ps1
+++ myfunctions/goto.ps1
@@ -5,95 +5,27 @@ function goto($dir_name) {
     # example:- goto py 
             switch ($dir_name) 
             {
-                "codes"
-                {
-                    #figlet("{code} Snippet")
-                    cd Get-Path("codes")
-                    dir
-                }
-                "profile"
-                {
-                    # figlet("PowerShell")
-                    cd Get-Path("PowerShell")
-                    dir
-                }
-<#                
-                "py" 
-                {
-                    figlet("PyScript")
-                    cd $path["pyscript"]
-                    dir
-                  
-                }
-#> 
-                "terminal" 
-                {
-                    #figlet("Terminal")
-                    cd Get-Path("terminal")
-                    dir
-                }
-<#                
-                "matrix"
-                {
-                    figlet "The Matrix"
-                    cd $path["mr_robot"]
-                    
-                }
-                "mrrobot" 
-                {
-                    figlet("Mr Robot")
-                    cd $goto_path["mr_robot"]
-                    dir
-                  
-                }
-                "oracle"
-                {
-                    figlet "The Oracle"
-                    cd $path["oracle"]
-                    dir
-                    
-                }
-                "pymath" 
-                {
-                     figlet("pymath")
-                     cd $path[$dir_name]
-                     dir *.bat             # Display only bat files
-                }
-                "project"
-                {
-                    figlet("Project")
-                    cd $path["project"]
-                    dir
-                }
-                "study"
-                {
-                    figlet("Study")
-                    cd $goto_path["study"]
-                    dir
-                }
-                "pico" 
-                {
-                    figlet("Raspberry Pico")
-                    cd $goto_path["pico"]
-                    dir
-                  
-                }
-                "payload" 
-                {
-                    figlet("Pico Payload")
-                    cd $goto_path["payload"]
-                    dir
-                  
-                }
-#>                
+                "codes"    { Goto-Directory("codes")}
+                "profile"  { Goto-Directory("powershell") }
+                "py"       { Goto-Directory("mypython")}
+                "terminal" { Goto-Directory("terminal") }
+                "matrix"   { Goto-Directory("matrix") }
+                "mrrobot"  { Goto-Directory("mrrobot") }
+                "oracle"   { Goto-Directory("oracle") }
+#               "pymath"   { Goto-Directory("terminal") }
+#               "project"  { Goto-Directory("terminal") }
+                "pico"     { Goto-Directory("pico") }
+                "payload"  { Goto-Directory("payload") }
+                "study"    { Goto-Directory("study") }
+                "scripts"  { Goto-Directory("myscripts") }
                 Default 
                 {
                     
                     Write-Host "[Invalid] You entered the directory[$dir_name] that is not on the profile dictionary"
-                    figlet 'goto'
+                    #figlet 'goto'
                     Write-Host "[Try] 'goto <directory-name> | with 'directory name from below' list"
-                    $goto_path.GetEnumerator() | Sort-Object | Format-Table @{label="Directory Name"; expression={$_.Key}}, @{label="Path"; expression={$_.Value}} -AutoSize
-    
+                    #$goto_path.GetEnumerator() | Sort-Object | Format-Table @{label="Directory Name"; expression={$_.Key}}, @{label="Path"; expression={$_.Value}} -AutoSize
+                    List-Path
                 }
             }
     
