2eb1fbbbe303185208b56ff878f34fe603258b3f
Sanjeev Stephan <samstephan164@gmail.com>
2023-04-03 09:05:02 +0530

    script to added new functions

---
+++ profileFunction/Add-My-Function.ps1
@@ -0,0 +1,6 @@
+function Add-MyFunction(){
+
+    $name = Read-Host "Enter The Function Name : "
+    $filename = Read-Host "Enter the Function Name"
+    $argument = Read-Host "Choose { Yes | NO }"
+}
