37d27ba9369f925e49371a756019accc5130cff3
Sanjeev Stephan <samstephan164@gmail.com>
2023-04-03 09:06:58 +0530

    added 'code to display the IP Address' in the prompt
    ┌──(superuser㉿192.168.29.1)-[C:\Users\Stephan]
    └─$ :

--- profileFunction/The-Prompt.ps1
+++ profileFunction/The-Prompt.ps1
@@ -1,11 +1,20 @@
+$ip_address = ipconfig | Select-String "IPv4 Address" | Select-Object -First 1 | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+
+function getip()
+{
+        return $ip_address
+}
+
 function prompt($display_msg) {
+
+    
     $location = '~'
     $location = Get-Location
     $computer_name = $env:COMPUTERNAME
     Write-Host '┌──(' -NoNewline -ForegroundColor Green
     Write-Host 'superuser' -NoNewline -ForegroundColor Yellow
     Write-Host '㉿' -NoNewline -ForegroundColor Red
-    Write-Host "$computer_name" -NoNewline -ForegroundColor Blue
+    Write-Host "$ip_address" -NoNewline -ForegroundColor Blue
     Write-Host ")-[$location]" -ForegroundColor Green
     Write-Host '└─$ ' -NoNewline -ForegroundColor Green
     $prompt_tail = ':'
