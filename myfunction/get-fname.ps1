ce4a8cdc2647b86c461d1b329db3ee26c74659d0
Sanjeev Stephan <samstephan164@gmail.com>
2023-04-17 21:17:47 +0530

    added 'sleepnow' to myfunctions

---
+++ myfunction/get-fname.ps1
@@ -0,0 +1,9 @@
+function get-fname() {
+$clipboardText = Get-Clipboard
+$modifiedText = $clipboardText.ToLower() -replace " ", "-"
+
+Write-Host "[ Copied ] from clipboard : $clipboardText"
+Write-Host "[ Pasted ] to   clipboard : $modifiedText"
+Set-Clipboard -Value $modifiedText
+
+}
