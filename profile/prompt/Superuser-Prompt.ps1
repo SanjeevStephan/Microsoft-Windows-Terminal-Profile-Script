baf1a0dede70ff980a3d5dab2bc68e41051426f0
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2023-06-02 00:10:33 +0530

    added 'SuperUser-Prompt'

--- profile/prompt/Superuser-Prompt.ps1
+++ profile/prompt/Superuser-Prompt.ps1
@@ -1,15 +1,24 @@
-function Superuser-Prompt($display_msg) {
+$ip_address = ipconfig | Select-String "IPv4 Address" | Select-Object -First 1 | ForEach-Object { $_.ToString().Split(":")[1].Trim() }
+
+function getip() { return $ip_address }
+
+function prompt($display_msg) {
+
+    
     $location = '~'
     $location = Get-Location
+    $computer_name = $env:COMPUTERNAME
     Write-Host '┌──(' -NoNewline -ForegroundColor Green
     Write-Host 'superuser' -NoNewline -ForegroundColor Yellow
     Write-Host '㉿' -NoNewline -ForegroundColor Red
-    Write-Host 'terminal' -NoNewline -ForegroundColor Blue
+    Write-Host "$ip_address" -NoNewline -ForegroundColor Blue
     Write-Host ")-[$location]" -ForegroundColor Green
     Write-Host '└─$ ' -NoNewline -ForegroundColor Green
     $prompt_tail = ':'
 
-    $choice = Read-Host $prompt_tail $display_msg
-    #Debug("You have entered $choice")
-    return $choice
+    #$command = Read-Host $prompt_tail $display_msg
+    #Debug("You have entered $command")
+
+    #Invoke-Expression $command 
+    #return $command 
 }
