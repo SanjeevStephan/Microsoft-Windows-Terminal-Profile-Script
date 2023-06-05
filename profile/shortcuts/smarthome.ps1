de43143b9cdc96b5c2d139b6458d59a4e6d39c0b
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2023-06-05 12:06:01 +0530

    renmed environment variable name from 'smarthome_console' to 'ctsmarthome_console'

--- profile/shortcuts/smarthome.ps1
+++ profile/shortcuts/smarthome.ps1
@@ -1,6 +1,6 @@
 function smarthome() {
     # $file = "ctSmarthome.PowerShell_console.ps1"
-    $filepath = $env:smarthome_console
+    $filepath = $env:ctsmarthome_console
     $file = Split-Path -Leaf $filepath
     $path = $filepath.Split($file)[0]
     cd $path
