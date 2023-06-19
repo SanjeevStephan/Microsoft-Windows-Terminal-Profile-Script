9bd35aefafe72cfd59fbcb25f597d66e663f3a43
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2023-06-20 01:06:18 +0530

    added nano.exe and copy-keepass

---
+++ profile/cmdlets/nano.ps1
@@ -0,0 +1,4 @@
+function nano($file)
+{
+    & $env:nano $file
+}
---
+++ profile/functions/edit-installer.ps1
@@ -0,0 +1,4 @@
+function autoedit() {
+    Set-Location C:\Users\Samst\OneDrive\Documents\GitHub\autoinstaller\store\
+    code autoinstaller.json
+}
