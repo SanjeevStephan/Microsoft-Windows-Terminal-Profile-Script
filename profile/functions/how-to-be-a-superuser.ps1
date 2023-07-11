49c6073862370ed3accee647a385809d560bc1c5
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2023-07-11 08:26:26 +0530

    help superuser

---
+++ profile/functions/how-to-be-a-superuser.ps1
@@ -0,0 +1,5 @@
+function how-to-be-a-superuser() {
+    Write-Host "Still Working...on that "
+    Write-Host "Kindly Check back after some-time"
+    set-location "C:\Users\Samst\OneDrive\Documents\PowerShell\profile\functions"
+}
